cd98fef66e850d7b5120dc3abd6cd06c2023c9f4
mahmudhanish@gmail.com <mahmudhanish@gmail.com>
2024-05-03 00:59:12 +0300

    clean up and update

---
+++ Service_Announcer.py
@@ -0,0 +1,32 @@
+import socket
+import json
+import time
+
+
+# Create a UDP socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+# Create a TCP socket for initiation
+initiationSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Server address and port
+serverUDPaddress = ('192.168.1.255', 6000)
+
+# Client ID
+client_id = input("Enter your ID: ")
+
+# Call this function to display available users
+username = json.dumps({'username': client_id})
+
+
+print(f"Welcome {client_id}")
+print("connecting to server")
+
+while True:
+    # Send periodic announcements to the server
+    client_socket.sendto(username.encode(), serverUDPaddress)
+
+
+    # Wait for a while before sending the next announcement
+    time.sleep(8)  # Adjust as needed for periodic announcements
