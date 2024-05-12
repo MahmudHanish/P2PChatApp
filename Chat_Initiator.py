8bd0335b68c41ec7feb72ea949e0597eb5490a14
mahmudhanish@gmail.com <mahmudhanish@gmail.com>
2024-05-12 15:14:17 +0300

    D O N E

--- Chat_Initiator.py
+++ Chat_Initiator.py
@@ -5,8 +5,6 @@ import os
 from cryptography.fernet import Fernet
 import base64
 
-
-
 with open('username.json', 'r') as file:
     user = json.load(file)
     username = user['username']
@@ -20,7 +18,9 @@ def display_available_users():
 
     current_time = time.time()
     available_users = "Available users:\n"
-    for username, client_info in data.items():
+    for user, client_info in data.items():
+        if user == username:
+            continue            
         last_seen = client_info[1]  # Get the last seen timestamp from the client info tuple
         if current_time - last_seen < 10:
             status = "(Online)"
@@ -28,19 +28,20 @@ def display_available_users():
             status = "(Away)"
         elif current_time - last_seen >= 900:
             del data[username]
-        available_users += f"{username} {status}\n"
+        available_users += f"{user} {status}\n"
     print(available_users)
-
 
 def save_chat_log(username, message, client):
     folder = 'logs'
     if not os.path.exists(folder):
         os.makedirs(folder)
     file_path = os.path.join(folder, f'{client}.txt')
-    with open(file_path,'a') as file:
+    with open(file_path, 'a') as file:
         file.write(f'{time.ctime()} | {username}: {message}\n')
 
-def history(client):
+def history():
+    client = input("Who's chat history do you want to see? ")
+
     folder = 'logs'
     if not os.path.exists(folder):
         print("no chat logs available")
@@ -55,56 +56,52 @@ def history(client):
 def chat():
     chatInitiator = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_ID = input("who do you want to chat with? ")
-    addr = (data[client_ID][0],6001)
+    addr = (data[client_ID][0], 6001)
     chatInitiator.connect(addr)
     secure = input("1 for encrypted chat, 2 for unencrypted chat: ")
+
+    # secure chat
     if secure == '1':
         secretKey = int(input("choose your secret key: "))
-        key = ((5^secretKey)%23)
-        json_payload = {"user": username,
-                        "key": key,
-                        }
+        key = ((5^secretKey) % 23)
+        json_payload = {"user": username, "key": key}
         chatInitiator.send(json.dumps(json_payload).encode())      
-        tempkey = int(chatInitiator.recv(2048).decode())
-        finalkey = (tempkey^secretKey)%23
-        integer_bytes = int.to_bytes(finalkey, length=32, byteorder='big')
-        fernet_key = base64.urlsafe_b64encode(integer_bytes)
-        f = Fernet(fernet_key)
+        tempkey = chatInitiator.recv(2048).decode()
+        if tempkey:
+            finalkey = (int(tempkey) ^ secretKey) % 23
+            integer_bytes = int.to_bytes(finalkey, length=32, byteorder='big')
+            fernet_key = base64.urlsafe_b64encode(integer_bytes)
+            f = Fernet(fernet_key)
 
-        message = input('message: ')
-        encrypted_message = Fernet.encrypt(message)
-        json_payload = {"user": username,
-                        "encrypted message": encrypted_message,
-                        }
-        chatInitiator.send(json.dumps(json_payload).encode())
-        save_chat_log(username, message, client_ID)
+            message = input('message: ')
+            encrypted_message_bytes = f.encrypt(message.encode())
+            encrypted_message = base64.b64encode(encrypted_message_bytes).decode()
+            json_payload = {"user": username, "encrypted_message": encrypted_message}
+            chatInitiator.send(json.dumps(json_payload).encode())
+            save_chat_log(username, message, client_ID)
+    
+    # unsecure chat
     elif secure == '2':
         message = input("message: ")
         key = 0
         unencrypted_message = message
-        json_payload = {"user": username,
-                        "key": key,
-                        "unencrypted message": unencrypted_message,
-                        }
+        json_payload = {"user": username, "key": key, "unencrypted_message": unencrypted_message}
+        print(message.encode())
         chatInitiator.send(json.dumps(json_payload).encode())
         save_chat_log(username, message, client_ID)
     chatInitiator.close()
-
-    
-
 
 def mainMenu():
     print("1. Users")
     print("2. Chat")
     print("3. History")
-    nav = input("what do you want to do? (use the numbers): ")
+    nav = input("What do you want to do? (use the numbers): ")
     if nav == '1':
         display_available_users()
     elif nav == '2':
         chat()
     elif nav == '3':
-        client = input("who's chat history do you want to see? ")
-        history(client)
+        history()
     else:
         print("invalid input")
 
